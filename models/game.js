0aaa943af717f87c430423f45956ddc7f2b66fb2
wysecw <cww22@students.uwf.edu>
2019-04-18 09:47:46 -0500

    set date display format

--- models/game.js
+++ models/game.js
@@ -2,20 +2,24 @@ const mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 let GameSchema = new Schema({
-    game_date: Date,
+    game_date: {
+        type: Date,
+        get: date => date.toDateString()
+    },
     game_time: String,
     field_number: Number,
-    visitors:{
-        team: {type: Schema.Types.ObjectId, ref: 'Team'},
+    visitors: {
+        team: { type: Schema.Types.ObjectId, ref: 'Team' },
         score: Number
     },
-    home:{
-        team: {type: Schema.Types.ObjectId, ref: 'Team'},
+    home: {
+        team: { type: Schema.Types.ObjectId, ref: 'Team' },
         score: Number
     }
 });
 
 //virtual function to calc winner/loser
+
 
 
 
